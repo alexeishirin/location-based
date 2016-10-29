c55cf02d4fd43b76bc75ab1d3b4a108be5bd8996
alexeishirin <alexei.shirin@gmail.com>
2016-10-29 23:41:49 +0300

    Persisting auth token and couple of helpers

---
+++ Assets/helpers/JSONProperty.cs
@@ -0,0 +1,13 @@
+﻿public class JSONProperty {
+	public string key;
+	public object value;
+
+	public JSONProperty (string key, object value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public string toJSONString () {
+		return "\"" + this.key + "\":\"" + this.value.ToString() + "\"";
+	}
+}
--- Assets/services/AuthService.cs
+++ Assets/services/AuthService.cs
@@ -3,12 +3,15 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Collections;
 using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using RSG;
 using UnityEngine.Networking;
 
 public class AuthService : BaseService<AuthToken>{
 	private static AuthService instance = null;
 	private AuthToken token = null;
+	private string AUTH_TOKEN_PERSIST_PATH;
 
 	public static AuthService getInstance () {
 		if (instance == null)
@@ -16,6 +19,8 @@ public class AuthService : BaseService<AuthToken>{
 			var gameObject = new GameObject("AuthService");
 			GameObject.DontDestroyOnLoad(gameObject);
 			instance = gameObject.AddComponent<AuthService>();
+			instance.AUTH_TOKEN_PERSIST_PATH = Application.persistentDataPath + "/auth.dat";
+			Debug.Log (instance.AUTH_TOKEN_PERSIST_PATH);
 		}
 
 		return instance;
@@ -26,12 +31,20 @@ public class AuthService : BaseService<AuthToken>{
 		loginObject.addProperty (new JSONProperty ("username", username));
 		loginObject.addProperty (new JSONProperty ("password", password));
 
-		return new Promise<AuthToken>((resolve, reject) =>
-			StartCoroutine(this.postRequestCoroutine(this.getServiceEndPoint() + "/login?include=user", loginObject.toJSONString(), resolve, reject))
-		);
+		return new Promise<AuthToken>((resolve, reject) => {
+			AuthToken persistedToken = this.loadPersistedAuthToken();
+			if (persistedToken != null) {
+				Action<AuthToken> newResolve = actionHelper.createSequence(this.setAuthToken, resolve);
+				newResolve(persistedToken);
+			} else {
+				Action<AuthToken> newResolve = actionHelper.createSequence(this.setAuthToken, this.persistAuthToken, resolve);
+				StartCoroutine(this.postRequestCoroutine(this.getServiceEndPoint() + "/login?include=user", loginObject.toJSONString(), newResolve, reject));
+			}
+		});
 	}
 
 	public void setAuthToken (AuthToken token) {
+		Debug.Log ("Auth token set");
 		this.token = token;
 	}
 	public AuthToken getAuthToken () {
@@ -42,6 +55,29 @@ public class AuthService : BaseService<AuthToken>{
 		if (this.token != null) {
 			request.SetRequestHeader ("Authorization", this.token.id);
 		}
+	}
+
+	public void persistAuthToken(AuthToken authToken) {
+		Debug.Log ("Auth token persisted");
+		BinaryFormatter binaryFormatter = new BinaryFormatter ();
+		FileStream file = File.Open (AUTH_TOKEN_PERSIST_PATH, FileMode.Create);
+		binaryFormatter.Serialize (file, authToken);
+		file.Close ();
+	}
+
+	public AuthToken loadPersistedAuthToken() {
+		if (!File.Exists (AUTH_TOKEN_PERSIST_PATH)) {
+			return null;
+		}
+
+		BinaryFormatter binaryFormatter = new BinaryFormatter ();
+		FileStream file = File.Open (AUTH_TOKEN_PERSIST_PATH, FileMode.Open);
+		AuthToken authToken = (AuthToken) binaryFormatter.Deserialize (file);
+		file.Close ();
+
+		Debug.Log ("Auth token succesfully loaded");
+
+		return authToken;
 	}
 }
 
